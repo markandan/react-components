1b484c721c45eeb69557695e727308cacd7c1dd0
Markandan <markr@thoughtworks.com>
2017-10-06 14:14:12 +0530

    [Mark] Configuration to webpack done and jest testing framework is included

--- src/index.js
+++ src/index.js
@@ -1,1 +1,4 @@
-console.log("Hello");
+function sum(a, b) {
+  return a + b;
+}
+module.exports = sum;
--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,55 @@
-var webpack = require('webpack');
-var path = require('path');
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
-var BUILD_DIR = path.resolve(__dirname, 'dist/');
-var APP_DIR = path.resolve(__dirname, 'src/');
-
-var config = {
-  entry: APP_DIR + '/index.js',
+module.exports = {
+  entry: {
+    "app": "./src/index.js"
+  },
   output: {
-    path: BUILD_DIR,
-    filename: 'bundle.js'
+    filename: 'scripts/[name].js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+     contentBase: './dist'
   },
   module: {
-    {
-      test : /\.jsx?/,
-      include : APP_DIR,
-      loader : 'babel-loader'
-    }
-  }
+     rules: [
+       {
+           test: /\.jsx?$/,
+           exclude: /node_modules/,
+           loader: 'babel-loader'
+       },
+       {
+           test: /(\.css|\.scss|\.sass)$/,
+           use: ExtractTextPlugin.extract({
+              fallback: 'style-loader',
+              //resolve-url-loader may be chained before sass-loader if necessary
+              use: ['css-loader', 'sass-loader']
+           })
+       },{
+          test: /\.(png|svg|jpg|gif)$/,
+          use: [
+            'file-loader'
+          ]
+        },
+       {
+         test: /\.(woff|woff2|eot|ttf|otf)$/,
+         use: [
+           'file-loader'
+         ]
+       }
+     ]
+   },
+   plugins: [
+     new CleanWebpackPlugin(['dist']),
+     new ExtractTextPlugin('style.css'),
+     new HtmlWebpackPlugin ({
+         template: 'index.ejs',
+         title: 'React Components'
+     })
+   ]
 };
-
-module.exports = config;
